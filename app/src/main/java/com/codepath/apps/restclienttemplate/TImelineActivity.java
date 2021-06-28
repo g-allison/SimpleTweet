3d01da69c1b554925eef97a65802969e6d37c7bd
g-allison <gallison@fb.com>
2021-06-28 19:44:07 -0400

    Creating a Timeline Activity and Row Layout

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TImelineActivity.java
@@ -0,0 +1,15 @@
+package com.codepath.apps.restclienttemplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TImelineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+
+    }
+}
